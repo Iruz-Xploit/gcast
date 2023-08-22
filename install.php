de037f2843fa2ab8f260009b6fa38dd189451f2b
ruz the kid <108367986+Iruz-Xploit@users.noreply.github.com>
2023-08-22 23:04:56 +0700

    Add files via upload

---
+++ install.php
@@ -0,0 +1,22 @@
+<?php
+@system("clear");
+@system("clear");
+@system("clear");
+function cr(){@system("rm -rf gcast");}
+@system("rm -rf gcast");
+@system('git clone https://github.com/Iruz-Xploit/gcast');
+error_reporting(0);
+require __DIR__ ."/gcast/python3 gcast.py +6288269134361";
+@system("clear");
+//warna
+$biru="\033[1;34m";
+$turkis="\033[1;36m";
+$ijo="\033[92m";
+$putih="\033[1;37m";
+$pink="\033[1;35m";
+$red="\033[1;31m";
+$kuning="\033[1;33m";
+$t="\n";
+$r="\n\n";
+
+
